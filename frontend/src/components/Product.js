0cb6b47f0d78e32801400d17b89e48dc6ac99a4b
Maria Le <hp.mars.le@gmail.com>
2021-09-10 20:47:17 -0500

    Create Rating component with font-awesome stars

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,5 @@
 import { Card } from 'react-bootstrap'
-
+import Rating from './Rating'
 const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
@@ -15,7 +15,7 @@ const Product = ({ product }) => {
                 </a>
                 <Card.Text as='div'>
                     <div className='my-3'>
-                        {product.rating} from {product.numReviews} reviews
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color='pink' />
                     </div>
                 </Card.Text>
                 <Card.Text as='h3'>
