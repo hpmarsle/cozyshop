f425f62078afd7c77f0e7b2034153715b83a69a9
Maria Le <hp.mars.le@gmail.com>
2021-09-13 22:40:44 -0500

    Initial cart functionality

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -21,13 +21,13 @@ export default function HomeScreen() {
         <>
             <h1>Latest Products</h1>
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :
-                <Row>
+                (<Row>
                     {products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                             <Product product={product} />
                         </Col>
                     ))}
-                </Row>}
+                </Row>)}
         </>
     )
 }
