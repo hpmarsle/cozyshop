8740226d705ed47474265e5248afb3ded589fcc2
Maria Le <hp.mars.le@gmail.com>
2021-09-14 20:47:49 -0500

    Change countInStock type from String to Number in productModel Schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -51,7 +51,7 @@ const productSchema = mongoose.Schema({
         default: 0
     },
     countInStock: {
-        type: String,
+        type: Number,
         required: true,
         default: 0
     }
