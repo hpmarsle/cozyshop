88be273d66ad286243dd0fbe8807416303ed8793
Maria Le <hp.mars.le@gmail.com>
2021-09-22 00:35:14 -0500

    Initial authentication for backend: More to come

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     name: {
@@ -23,6 +24,9 @@ const userSchema = mongoose.Schema({
     timestamps: true
 })
 
+userSchema.methods.matchPassword = async function (enteredPW) {
+    return await bcrypt.compare(enteredPW, this.password)
+}
 const User = mongoose.model('User', userSchema)
 
 export default User
