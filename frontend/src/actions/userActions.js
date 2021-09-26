68cadf02b446e4b02ece272207fa3bcb399f5099
Maria Le <hp.mars.le@gmail.com>
2021-09-26 01:24:59 -0500

    User login screen and functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
 
-export const login = (email, password) => async(dispatch => {
+export const login = (email, password) => async (dispatch) => {
     try {
         dispatch({ type: USER_LOGIN_REQUEST })
 
@@ -33,4 +33,4 @@ export const login = (email, password) => async(dispatch => {
         })
     }
 
-})
+}
