fdca48f37f93f46696652a94a50d22cf36590875
Maria Le <hp.mars.le@gmail.com>
2021-09-26 20:28:35 -0500

    Create update profile PUT endpoint to update user info

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 const router = express.Router()
 import { protect } from '../middleware/authMiddleware.js'
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
 
 router.route('/').post(registerUser).get(protect)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
 
 export default router
