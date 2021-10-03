005ef58d44c784ae789471b294246d2b83bd57dd
Maria Le <hp.mars.le@gmail.com>
2021-10-03 00:18:57 -0500

    Create orderDetails reducer and action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,6 +1,11 @@
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL} from '../constants/orderConstants'
-
-import React from 'react'
+import { 
+    ORDER_CREATE_REQUEST, 
+    ORDER_CREATE_SUCCESS, 
+    ORDER_CREATE_FAIL, 
+    ORDER_DETAILS_REQUEST, 
+    ORDER_DETAILS_SUCCESS, 
+    ORDER_DETAILS_FAIL
+} from '../constants/orderConstants'
 
 const orderCreateReducer = (state = {}, action) => {
     switch(action.type){
@@ -24,4 +29,29 @@ const orderCreateReducer = (state = {}, action) => {
     }
 }
 
-export {orderCreateReducer}
+const orderDetailsReducer = (
+    state = { loading: true, orderItems: [], shippingAddress: {} },
+    action
+  ) => {
+    switch (action.type) {
+      case ORDER_DETAILS_REQUEST:
+        return {
+          ...state,
+          loading: true,
+        }
+      case ORDER_DETAILS_SUCCESS:
+        return {
+          loading: false,
+          order: action.payload,
+        }
+      case ORDER_DETAILS_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        }
+      default:
+        return state
+    }
+  }
+
+export {orderCreateReducer, orderDetailsReducer}
