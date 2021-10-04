e5bfef3d7619642aadfe712da8d46dfe2d091b29
Maria Le <hp.mars.le@gmail.com>
2021-10-04 03:20:38 -0500

    Add PayPal payments functionality

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -4,7 +4,11 @@ import {
     ORDER_CREATE_FAIL, 
     ORDER_DETAILS_REQUEST, 
     ORDER_DETAILS_SUCCESS, 
-    ORDER_DETAILS_FAIL
+    ORDER_DETAILS_FAIL,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_RESET
 } from '../constants/orderConstants'
 
 const orderCreateReducer = (state = {}, action) => {
@@ -54,4 +58,30 @@ const orderDetailsReducer = (
     }
   }
 
-export {orderCreateReducer, orderDetailsReducer}
+const orderPayReducer = (
+    state = { loading: true, orderItems: [], shippingAddress: {} },
+    action
+  ) => {
+    switch (action.type) {
+      case ORDER_PAY_REQUEST:
+        return {
+          loading: true,
+        }
+      case ORDER_PAY_SUCCESS:
+        return {
+          loading: false,
+          success: true
+        }
+      case ORDER_PAY_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        }
+      case ORDER_PAY_RESET:
+        return {}
+      default:
+        return state
+    }
+  }
+
+export {orderCreateReducer, orderDetailsReducer, orderPayReducer}
