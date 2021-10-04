a49cd4a3dd6940ac6e61661bb46e191bb2940935
Maria Le <hp.mars.le@gmail.com>
2021-10-04 00:26:45 -0500

    Add endpoint to update order to paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,6 +36,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder)
   }
 })
+
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
@@ -50,7 +51,33 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if(order){
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResule = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address
+    }
+
+    const updateOrder = await order.save()
+    
+    res.json(updatedOrder)
+  }else {
+    res.status(404)
+    throw new Error('order not found')
+  }
+})
+
 export {
   addOrderItems,
-  getOrderById
+  getOrderById,
+  updateOrderToPaid
 }
