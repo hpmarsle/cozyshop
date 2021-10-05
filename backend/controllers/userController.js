dd5e01d1c377645269dce4902fb70b6c6d204c57
Maria Le <hp.mars.le@gmail.com>
2021-10-05 03:08:38 -0500

    Add GET users endpoint for admin access

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,6 +75,13 @@ const getUserProfile = asyncHandler(async (req, res) => {
         throw new Error('User not found')
     }
 })
+// @desc Get all users 
+// @route GET /api/users
+// @access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+})
 
 // @desc Update user profile
 // @route PUT /api/users/profile
@@ -106,4 +113,4 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 })
 
 
-export { authUser, getUserProfile, registerUser, updateUserProfile }
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { protect } from '../middleware/authMiddleware.js'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile } from '../controllers/userController.js'
 
-router.route('/').post(registerUser).get(protect)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile')
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile)
-
 export default router
