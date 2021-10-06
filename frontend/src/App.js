9712ba291036a64c8aed659fbdecbe53d67375b9
Maria Le <hp.mars.le@gmail.com>
2021-10-06 01:22:47 -0500

    Create UserList Screen for admin view of registered users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import OrderScreen from './screens/OrderScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import UserListScreen from './screens/UserListScreen'
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
             <Route path='/register' component={RegisterScreen} />
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
+            <Route path='/admin/userList' component={UserListScreen} />
             <Route exact path='/' component={HomeScreen} />
           </Container>
         </main>
