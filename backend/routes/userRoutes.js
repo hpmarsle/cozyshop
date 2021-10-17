233871537aa03a13b9209da22dad35f6680e16c2
Maria Le <hp.mars.le@gmail.com>
2021-10-16 23:56:27 -0500

    Add PUT and GET user by id endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express'
 const router = express.Router()
 import { protect, admin } from '../middleware/authMiddleware.js'
-import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile, deleteUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile, deleteUser, updateUser, getUserById } from '../controllers/userController.js'
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile')
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
+router.route('/:id')
+    .get(protect, admin, getUserById)
+    .delete(protect, admin, deleteUser)
+    .put(protect, admin, updateUser)
 
 export default router
