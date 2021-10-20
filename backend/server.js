871fe45b78c7fdcbd4caca9dcf470609eecd577f
Maria Le <hp.mars.le@gmail.com>
2021-10-20 03:29:46 -0500

    Admin user is able to upload product image

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
@@ -7,6 +8,7 @@ import { protect } from './middleware/authMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -17,20 +19,30 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
+	res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
-    res.send(process.env.PAYPAL_CLIENT_ID)
+	res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 app.use(notFound)
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(
+	PORT,
+	console.log(
+		`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
+)
