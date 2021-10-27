d209b6504142aa028ad806fd684fd52afb24b6cd
Maria Le <hp.mars.le@gmail.com>
2021-10-27 01:25:41 -0500

    Admin can mark an order as delivered on the Order Screen when the order is paid

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -7,11 +7,13 @@ import {
 	updateOrderToPaid,
 	getMyOrders,
 	getOrders,
+	updateOrderToDelivered,
 } from '../controllers/orderController.js'
 
 router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 export default router
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -16,6 +16,10 @@ import {
 	ORDER_LIST_REQUEST,
 	ORDER_LIST_SUCCESS,
 	ORDER_LIST_FAIL,
+	ORDER_DELIVER_REQUEST,
+	ORDER_DELIVER_SUCCESS,
+	ORDER_DELIVER_FAIL,
+	ORDER_DELIVER_RESET,
 } from '../constants/orderConstants'
 
 const orderCreateReducer = (state = {}, action) => {
@@ -91,6 +95,32 @@ const orderPayReducer = (
 	}
 }
 
+const orderDeliverReducer = (
+	state = { loading: true, orderItems: [], shippingAddress: {} },
+	action
+) => {
+	switch (action.type) {
+		case ORDER_DELIVER_REQUEST:
+			return {
+				loading: true,
+			}
+		case ORDER_DELIVER_SUCCESS:
+			return {
+				loading: false,
+				success: true,
+			}
+		case ORDER_DELIVER_FAIL:
+			return {
+				loading: false,
+				error: action.payload,
+			}
+		case ORDER_DELIVER_RESET:
+			return {}
+		default:
+			return state
+	}
+}
+
 const orderListMyReducer = (state = { loading: true, orders: [] }, action) => {
 	switch (action.type) {
 		case ORDER_LIST_MY_REQUEST:
@@ -140,6 +170,7 @@ export {
 	orderCreateReducer,
 	orderDetailsReducer,
 	orderPayReducer,
+	orderDeliverReducer,
 	orderListMyReducer,
 	orderListReducer,
 }
