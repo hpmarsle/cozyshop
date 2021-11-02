fb1bc9ad12282d2a25bd8411df5f7b6832897213
Maria Le <hp.mars.le@gmail.com>
2021-11-02 17:21:43 -0500

    Display page links with Paginate component using Pagination from react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,16 @@ const App = () => {
 						<Route path='/product/:id' component={ProductScreen} />
 						<Route path='/cart/:id?' component={CartScreen} />
 						<Route path='/admin/userList' component={UserListScreen} />
-						<Route path='/admin/productList' component={ProductListScreen} />
+						<Route
+							exact
+							path='/admin/productList'
+							component={ProductListScreen}
+						/>
+						<Route
+							exact
+							path='/admin/productList/:pageNumber'
+							component={ProductListScreen}
+						/>
 						<Route path='/admin/orderList' component={OrderListScreen} />
 						<Route path='/admin/user/:id/edit' component={UserEditScreen} />
 						<Route
